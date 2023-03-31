7dc3452864bb501a0f8768784926152df4f9121f
tangbo <admin@tbphp.net>
2023-03-31 15:31:57 +0800

    feat:gorm日志

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -2,14 +2,25 @@ package database
 
 import (
 	"fmt"
+	log "github.com/sirupsen/logrus"
 	"github.com/tbphp/go-edusys/pkg/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"time"
 )
 
 var DB *gorm.DB
 
 func init() {
+	// 配置日志
+	newLogger := logger.New(log.StandardLogger(), logger.Config{
+		SlowThreshold:             200 * time.Millisecond,
+		LogLevel:                  logger.Warn,
+		IgnoreRecordNotFoundError: true,
+		Colorful:                  true,
+	})
+
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=%s",
 		config.Database.Host,
@@ -20,7 +31,9 @@ func init() {
 		config.Database.TimeZone,
 	)
 	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
 	if err != nil {
 		panic(err)
 	}
